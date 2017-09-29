5218c6684e40f153b3a58e8c2bd3d6ebd69ea5a0
Rom Grk <romgrk.cc@gmail.com>
2017-09-29 11:36:13 -0400

    make things work (dev mode)

---
+++ helpers/handlers.js
@@ -0,0 +1,15 @@
+/*
+ * handlers.js
+ */
+
+
+exports.errorHandler = res => err => {
+  res.json({ ok: false, message: err.toString(), stack: err.stack.split('\n') })
+  res.end()
+}
+
+exports.okHandler = res => data => {
+  res.json({ ok: true, data: data })
+  res.end()
+}
+
