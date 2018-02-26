6eaa9cb41e4fbd801c493bff94a511649fc56b9b
Rom Grk <romgrk.cc@gmail.com>
2018-02-26 18:07:24 -0500

    update CSP

--- app.js
+++ app.js
@@ -25,7 +25,9 @@ app.set('view engine', 'jade')
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(logger('dev'))
 app.use(helmet())
-app.use(csp({ policies: { 'default-src': [csp.SELF] } }))
+app.use(csp({ policies: {
+  'default-src': [csp.SELF, 'https://fonts.googleapis.com']
+} }))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
