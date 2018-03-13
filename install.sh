855fbb6208340440415510cbe9afec67ba793bb4
Rom Grk <romgrk.cc@gmail.com>
2018-03-13 16:45:55 -0400

    installer: test that nodejs+git exist

--- install.sh
+++ install.sh
@@ -13,6 +13,7 @@ did_finish=0
 red=$(echo -e '\x1b[1;91m')
 green=$(echo -e '\x1b[1;92m')
 blue=$(echo -e '\x1b[1;38;5;39m')
+bold=$(echo -e '\x1b[1m')
 reset=$(echo -e '\x1b[0m')
 
 function onExit {
@@ -26,8 +27,19 @@ function onExit {
 trap onExit EXIT
 
 
+# Test that git and nodejs are installed
+if [[ -z $(command -v git) ]]; then
+  echo "${red}Git is not installed or is not in path.${reset}"
+  exit 1
+fi
+if [[ -z $(command -v node) ]] || [[ -z $(command -v npm) ]]; then
+  echo "${red}NodeJS/NPM is not installed or is not in path.${reset}"
+  exit 1
+fi
+
+
 # Print informations
-echo "Installation directory: $install_dir"
+echo "${bold}Installation directory: $install_dir${reset}"
 
 
 # Clone & install packages
