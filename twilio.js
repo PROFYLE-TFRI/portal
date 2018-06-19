270fff431b4eb13c1546012261b1b5646cc9f2ca
Rom Grk <romgrk.cc@gmail.com>
2018-06-19 13:45:44 -0400

    skip twilio when not configured

--- twilio.js
+++ twilio.js
@@ -6,13 +6,16 @@
 const twilio = require('twilio')
 const config = require('./config')
 
-const client = twilio(config.twilio.account, config.twilio.token)
+const client = config.twilio.account && config.twilio.token ? twilio(config.twilio.account, config.twilio.token) : undefined
 
 module.exports = {
   sendMessage,
 }
 
 function sendMessage(to, body) {
+  if (client === undefined)
+    throw new Error('Twilio account not configured in config.js')
+
   return client.messages.create({
     from: config.twilio.from,
     to,
